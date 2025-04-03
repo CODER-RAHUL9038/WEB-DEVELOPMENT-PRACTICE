49c2fc3df890d173c473ad03c2cb126c1fe5b674
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-04 00:15:34 +0530

    add javacript folder

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,76 @@
+// let pencil  = 10;
+// let eraser  =5;
+// // NORMAL WAY TO CONCATENATE STRINGS AND EXPRESSION
+// // output= "The total price is :"+ (pencil+eraser)  + " Rupees"
+// // USING TEMPLATE LITERALS
+// output = `The total price is ${pencil+eraser}  Rupees`
+// console.log(output)
+
+// ARiTHMETIC OPERATORlet 
+// let a = 10;
+// let b = 5;
+
+// console.log(a+b)
+// console.log(a%b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+// console.log(++a) // 11
+// console.log(a++)//10
+
+// let age =17;
+// if (age>=18){
+//     console.log("You can vote")
+// }
+// if (age<18) {
+//     console.log("You cannot vote!")
+// }
+
+// TRAFFIC LIGHT SYSTEM
+let color = "red"
+if (color=="green") {
+    console.log("Go!")
+    
+}
+else if (color=="yellow") {
+    console.log("Go Slow!")
+    
+}
+else if (color=="red") {
+    console.log("Stop!")
+    
+}
+
+
+// let age =17;
+//  if (age>=18){
+//      console.log("You can vote")
+// }
+// else if (age < 18){
+//     console.log("You cannot vote")
+// }
+// let marks = ;
+// if (marks>=80) {
+//     console.log("You got E")
+// }
+
+
+//  else if (marks>=70) {
+//     console.log("You got A+")
+// }
+//  else if (marks>=50) {
+//     console.log("You got B")
+// }
+//  else if (marks>=33) {
+//     console.log("You got C")
+// }
+//  else if (marks<33) {
+//     console.log("You Failed")
+// }
+let month = "april"
+if (month=="janauary") {
+    console.log("winter is here")
+}
+else if(month=="april"){
+    console.log("welcome to Summer")
+}
