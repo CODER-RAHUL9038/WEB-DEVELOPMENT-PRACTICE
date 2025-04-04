1ace26fe44d203aecb9e4581264142e8df7c78d3
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-04 23:57:57 +0530

    added conditional statements,operators etc

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -27,19 +27,19 @@
 // }
 
 // TRAFFIC LIGHT SYSTEM
-let color = "red"
-if (color=="green") {
-    console.log("Go!")
+// let color = "red"
+// if (color=="green") {
+//     console.log("Go!")
     
-}
-else if (color=="yellow") {
-    console.log("Go Slow!")
+// }
+// else if (color=="yellow") {
+//     console.log("Go Slow!")
     
-}
-else if (color=="red") {
-    console.log("Stop!")
+// }
+// else if (color=="red") {
+//     console.log("Stop!")
     
-}
+// }
 
 
 // let age =17;
@@ -67,10 +67,73 @@ else if (color=="red") {
 //  else if (marks<33) {
 //     console.log("You Failed")
 // }
-let month = "april"
-if (month=="janauary") {
-    console.log("winter is here")
+// let month = "april"
+// if (month=="janauary") {
+//     console.log("winter is here")
+// }
+// else if(month=="april"){
+//     console.log("welcome to Summer")
+// }
+// let age =18;
+// if (age>=18) {
+//     console.log("You can vote")
+// }
+// else{
+//     console.log("You cannot Vote")
+// }
+
+// traffic light with if and else
+//  let light = "1"
+// if (light === "green") {
+//     console.log("Go Ahead!")
+//  }
+//  else if (light === "yellow") {
+//     console.log("Go slow!")
+//  }
+//  else if (light === "red") {
+//     console.log("Stop!")
+//  }
+// else{
+//     console.log("The light is Broken")
+//  }
+
+// Popcorn price based on based on their sizes
+
+// let size = ""
+// if (size === "xl") {
+//     console.log("Price is Rs. 250")
+// }
+// else if (size === "l") {
+//     console.log("Price is Rs. 200")
+// }
+// else if (size === "m") {
+//     console.log("Price is Rs. 100")
+// }
+// else  {
+//     console.log("Price is Rs. 50")
+// }  
+
+// NESTED IF else
+// let marks = 95;
+// if (marks >=33) {
+//     console.log("Pass")
+//     if (marks >= 80) {
+//         console.log("Your grade is: O (outstanding)")
+//     }
+//     else{
+//         console.log("The grade is: A")
+//     }
+// }
+// else{
+//     console.log("Failed")
+// }
+
+// console.log( (5>3 && 4>5 ))
+
+let str = "apple";
+if ((str[0]=="a") && (str.length > 3)) {
+    console.log("String is good");
 }
-else if(month=="april"){
-    console.log("welcome to Summer")
+else {
+    console.log("string is bad");
 }
