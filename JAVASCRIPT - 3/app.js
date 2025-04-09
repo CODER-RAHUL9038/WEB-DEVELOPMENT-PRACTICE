594c7d5cce8c81fa55cd2a2eda92255de814765d
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-10 00:13:24 +0530

    Add Javascript-3

---
+++ JAVASCRIPT - 3/app.js
@@ -0,0 +1,25 @@
+// Array 
+//  let students = ["Student1", "student2","student3"]
+//  console.log(students)
+
+// let nums = [2,4,6,8]
+// console.log(nums[0])
+// console.log(nums.length)
+// console.log(typeof(nums ))
+
+// STORING ARRAY
+
+// let numbers = [25, 65 ,45 ,89]
+// let students = ["Rahul", "Aman", "rohan"]
+// let info = [25 ,67 ,98, "coding", "namchi",76]
+// console.log(info)
+
+    // EMPTY ARRAY
+    // let info = ["coding", "namchi"];
+    // console.log(info)
+
+    // ARRAYS ARE MUTABLE
+    let fruits = ["banana","mango","orange"]
+    console.log(fruits) 
+    fruits[0] = "apple"
+    console.log(fruits)
