4909671f264a0b3000a0bdeb3b06c666567e78db
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-10 00:25:15 +0530

    added Javascript -2

---
+++ JAVASCRIPT -2/app.js
@@ -0,0 +1,49 @@
+// STRING METHODS
+
+// let msg = (" Hello ")
+// console.log(msg)
+// msg.trim() 
+ 
+// let pasword = prompt ("Enter Password");
+// n1 = pasword
+
+// console.log(n1);
+// console.log(pasword.trim());
+
+// UPPERCASE AND LOWERCASE
+
+// let str = "Check"
+// a = (str.toUpperCase())
+// console.log(a)
+
+// INDEXOF
+// let msg = " Hello "
+// let newMsg = msg.trim().toUpperCase()
+// console.log(newMsg)
+
+// STRING SLICE
+
+// let str = "Ilovecoding"
+// console.log(str.slice(-5 ))
+
+// Replace
+// let str = "Ilovecoding"
+// let newStr = str.replace("love","do")
+// console.log(newStr)
+
+// REPEAT
+
+// let str = "Rahul"
+// console.log(str.repeat(4))
+
+// PRACICE QUESTIONS
+1.
+let msg  = " help! "
+console.log(msg.trim().toUpperCase())
+
+2
+let Name = "ApnaCollege"
+console.log(Name.slice(4,9))
+console.log(Name.indexOf("na"))
+console.log(Name.slice(4).replace("l","t"))
+ 
