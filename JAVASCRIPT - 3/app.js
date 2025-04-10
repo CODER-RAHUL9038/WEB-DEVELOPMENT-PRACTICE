20cf6a8f2400298a28aa796cc73e86394922a21e
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-10 21:32:37 +0530

    added array

--- JAVASCRIPT - 3/app.js
+++ JAVASCRIPT - 3/app.js
@@ -19,7 +19,67 @@
     // console.log(info)
 
     // ARRAYS ARE MUTABLE
-    let fruits = ["banana","mango","orange"]
-    console.log(fruits) 
-    fruits[0] = "apple"
-    console.log(fruits)
+    // let fruits = ["banana","mango","orange"]
+    // console.log(fruits) 
+    // fruits[0] = "apple"
+    // console.log(fruits)
+
+    // ARRAY METHODS
+
+// let str = ["audi","bmw","mercedes"]
+// // PUSH (PUSH TO END )
+// str.push("ferrari")
+// console.log(str)
+// // POP (DELETE FROM END)
+// str.pop()
+// console.log(str)
+// // UNSHIFT (PUSH TO FRONT)
+// str.unshift("ferrari")
+// console.log(str)
+// // SHIFT (DELETE FROM FRONT)
+// str.shift()
+// console.log(str)
+
+
+// let followers = ["a","b","c"];
+// let blocked = followers.shift()
+// console.log(followers)
+// console.log(blocked)
+
+// ADDING AND DELETING FROM TEH FRONT - 1ST APPROCH
+// let start = [ "janauary","july","march","august"]
+// start.shift()
+// start.unshift("july")
+// start[1] = "june"
+// console.log(start)
+
+// adding and deleting from the front end 2ND APPROACH
+
+// let start = [ "janauary","july","march","august"]
+// start.shift()
+// start.shift()
+// start.unshift("july")
+// start.unshift("june")
+// console.log(start)
+
+// INDEX OF IN ARRAY
+// let primary =[ "red", "red","yellow"]
+// console.log(primary.indexOf("red")) // 0
+// console.log(primary.indexOf("Red")) // -1
+// console.log(primary.indexOf("yellow")) // 2
+
+
+// INCLUDES IN ARRAY
+// console.log(primary.includes("hELLO")) // false
+// console.log(primary.includes("red")) // true
+
+// CONCATENATION IN ARRAYS
+//  let primary = [1 ,2 ,3]
+//  let secondary = [4, 5, 6]
+//  let final = primary.concat(secondary)
+//  console.log(final)
+
+// REVERSE IN ARRAY
+// let primary = [1 ,2 ,3]
+// primary.reverse();
+// console.log(primary)
