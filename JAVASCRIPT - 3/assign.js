8fae838a6d504f22f504a2694570f114a787a53c
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-14 19:13:05 +0530

    Added Assignment

---
+++ JAVASCRIPT - 3/assign.js
@@ -0,0 +1,48 @@
+
+// 1. PRINT FIRST N POSTIVE NUMBER OF AN ARRAY
+
+// 2. PRINT LAST N POSTIVE NUMBER OF AN ARRAY
+
+// 3. PROGRAM TO CHECK IF A STRING IS EMPTY OR NOT
+// let strn = ""
+// if (strn.length == 0) {
+//     console.log("The string is empty")
+// }
+// else{
+//     console.log("The strin is not empty")
+// }
+
+//4. CHECK IF A CHARACTER IF LOWERCASE
+// let letter = "RAHUL"
+// if (letter == letter.toLowerCase()) {
+//     console.log("The character is in lowercase")
+// }
+// else{
+//     console.log("The character is not in Lowercase")
+// }
+
+// 5. PROGRAM TO CHECK LEADINFG AND TRAILING SPACES FROM A  STRING
+let  str = "  hello  "
+console.log(str.trim())
+
+6. // CHECK IF ANY ELEMENT IN PRESENT IN ARRAY OR NOT USING INCLUDES
+
+// let arr = [24,52,98,65,2]
+// idx = 24
+// if (arr.includes(24)) {
+//     console.log("The element is present")
+// }
+// else{
+//     console.log("The element is not present")
+// }
+
+6. // CHECK IF ANY ELEMENT IN PRESENT IN ARRAY OR NOT USING INDEXOF
+
+// let arr = [24,52,98,65,2]
+// idx = Number(prompt("Enter the Value"))
+// if (arr.indexOf(24) !== -1) {
+//     console.log("The element is present")
+// }
+// else{
+//     console.log("The element is not present")
+// }
