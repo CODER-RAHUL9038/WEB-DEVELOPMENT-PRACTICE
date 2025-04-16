78880a1acb01957754d736bed8c90c87a6c14146
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-16 23:39:44 +0530

    Add Movie Guesser Game using while LOOP

--- JAVASCRIPT -4/app.js
+++ JAVASCRIPT -4/app.js
@@ -1,4 +1,112 @@
-// FOR LOOP 
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
+// FOR LOOP
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 5; i >= 5; i++) {
+//     console.log(i);
+// }
+
+// PRINT ALL ODD NUMBERS BETWEEN 1-15
+
+// for( let i = 1; i<=15;i=i+2)
+//     console.log(i)
+
+// PRINT ALL EVEN NUMBERS BETWEEN 1-10
+
+// for (let i = 2; i <=10; i= i+2) {
+//     console.log(i);
+// }
+
+// PRINT ALL EVEN NUMBERS BETWEEN 1-10 BACKWARDS
+
+// for (let i = 10; i>=2; i= i-2) {
+//     console.log(i);
+// }
+
+// PRINT ALL ODD NUMBERS BETWEEN 1-15 BACKWARDS
+
+// for( let i = 15; i>=1; i=i-2)
+// console.log(i)
+
+// PRINT ALL MULTIFICATION TABLE OF 5
+// n= Number(prompt("Enter the Number"))
+// for( let i = n; i<=(n*10); i=i+n)
+// console.log(i)
+
+
+// NESTED FOR LOOP
+
+// for (let i =1; i<=3; i++){
+//     console.log(`Outer Loop ${i}`) // will tell outer loop no
+//     for (let j =1; j<=3; j++){
+//         console.log(j)
+//     }
+// }
+
+// WHILE LOOP
+// // PRINT 1- 5
+// let i =1;
+// while (i<=5) {
+//     console.log(i)
+//     i++
+// }
+
+// PRINT 1- 5  BACKWARDS
+// let i =5;
+//         while (i>=1) {
+//             console.log(i)
+//             i--
+//         }
+
+// PRINT 1- 20  BACKWARDS
+
+// let i =1;
+// while (i<=20) {
+//     console.log(i)
+//     i++
+// }
+
+// GUESS THE FAVOURITE MOVIE NAME AND USER ENTERS "QUIT " AND CAN QUIT THE GAME
+
+// let userInput = prompt("Enter the movie name (Type 'Quit' to exit)")
+// let movie = "pk"
+// while (userInput !== movie) {
+//    if (userInput == "quit"){
+//         console.log("logged out")
+//         break
+//     }  
+//    else{
+//         userInput = prompt(" Wrong Input! Enter the Movie name again")
+//     }
+// }
+// if (userInput == movie){
+//     console.log(" You are corect!")
+// }
+
+
+// ✅ JS Game: Limited Attempts Movie Guessing
+let movie = "pk"
+let attempts = 3;
+let userInput;
+while (attempts>0){
+    userInput = prompt(`Enter the movie name (Attempts left: ${attempts}) or type 'quit' to exit:`);
+    if (userInput == movie) {
+        console.log(`Woah! ${userInput} is correct answer`)
+        break;
+    }
+    else if (userInput == "quit"){
+        console.log("You are logged out")
+        break;
+    }
+    else {
+        attempts --
+        if (attempts == 0) {
+            console.log("🚫 No attempts left. Game over!");
+        } else {
+            console.log(`${userInput} is Wrong input. Try again`);
+        }
+    }
 }
+
+
