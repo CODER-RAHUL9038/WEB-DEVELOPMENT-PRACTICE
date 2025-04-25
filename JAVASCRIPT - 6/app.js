6af53abc0eca8ee5a3b83e7654dd95bbdc2bc75d
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-25 23:33:29 +0530

    added function

--- JAVASCRIPT - 6/app.js
+++ JAVASCRIPT - 6/app.js
@@ -6,3 +6,32 @@ function hello() {
 
 // CALLING FUNCTION
 hello();
+
+// FUNTION -2 
+function printName() {
+    console.log("My Name Is Rahul")
+}
+printName()
+
+
+// FUNTION -3
+function print1to5() {
+    for (i=0; i<=5; i++){
+        console.log(i)
+    }
+}
+print1to5()
+
+
+// FUNTION -4
+function isAdult() {
+    let age = 59;
+    if (age >= 18){
+        console.log("Adult")
+    }
+    else{
+        console.log("Not Adult")
+    }
+}
+
+isAdult()
