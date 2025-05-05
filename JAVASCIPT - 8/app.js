49eb9358bf24693e3b42bb0d8a16186a28635715
Rahul Shaw <rahulshaw903866@gmail.com>
2025-05-05 23:28:27 +0530

    added higher order array methods

--- JAVASCIPT - 8/app.js
+++ JAVASCIPT - 8/app.js
@@ -104,7 +104,7 @@
 // });
 // console.log(ans); // (5) [56, 12, 20, 56, 90] all even
 
-// *********** EVERY  ************************* 
+// *********** EVERY = LOGICAL AND  ************************* 
 
 // EVEN 
 
@@ -116,8 +116,84 @@
 
 
 // ODD 
-let num = [1,3,5]
-let result = num.every((el)=>{
-    return (el % 2 !== 0) //ODD  
-})
-console.log(result)
+// let num = [1,3,5]
+// let result = num.every((el)=>{
+//     return (el % 2 !== 0) //ODD  
+// })
+// console.log(result)
+
+// *********** SOME = LOGICAL OR  ************************* 
+
+// let num = [1,3,4,2]
+
+// let result = num.some ((el) =>{
+//     return (el % 2 == 0) //Even
+
+    
+// })
+
+// console.log(result)
+
+// *********** REDUCE = CONVERT TO INGLE VALUE  ************************
+// let num  = [1,2,3,4]
+// let result = num.reduce((res, el) =>{
+//     return res+el
+    
+// });
+// console.log(result) // sum of all element of array
+
+// MAX NO IN AN ARRAY USING REDUCE METHOD
+
+// let num  = [10,2,3,4,50]
+// let result = num.reduce((max,el) =>{
+//     if(el > max){
+//         return el
+//     }
+//     else{
+//         return max
+//     }
+// })
+// console.log(result)
+
+
+// USING TERNARY OPERATOR
+// let num  = [10,2,3,4,50]
+// let result = num.reduce((max, el) => (max< el ? el: max)) // curly bracket not used as not using only return
+// console.log(result)
+
+
+// CHECK IF ALL ELEMENTS IN AN ARRAY IS MULTIPLE OF 10 OR NOT
+// FOR THIS WE NEED EVERY METHOD
+
+// let num = [ 20,50,40,500]
+// let result = num.every((el) =>{
+//     return el % 10 == 0
+// });
+// console.log(result)
+
+// CREATA A FUNCTION TO FIND THE MINIMUM NO IN AN ARRAY USING REDUCE
+
+// let num = [ 10,20,30,5]
+//  let getMinimum = function(num){
+//     let result = num.reduce((min,el) =>{
+//         if(min > el){
+//             return el
+//         }
+//         else{
+//             return min
+//         }
+//     })
+//     return result
+//  }
+// console.log(getMinimum(num))
+
+    // SAME PROBLEM USING TERNARY OPERATOR
+
+//  let num = [ 10,20,30,5]
+//  let getMinimum = function(num){
+//     let result = num.reduce((min, el) => min > el? el : min )
+//     return result
+//  }
+//  console.log(getMinimum(num))
+
+
