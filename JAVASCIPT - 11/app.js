30d4157d0b5af57094bef50a0fe8a5b855e19da5
Rahul Shaw <rahulshaw903866@gmail.com>
2025-05-26 18:44:23 +0530

    added Todo App

--- JAVASCIPT - 11/app.js
+++ JAVASCIPT - 11/app.js
@@ -1,21 +1,61 @@
 // EVENT BUBBLING
 
-let div =document.querySelector('div')
+// let div =document.querySelector('div')
+// let ul = document.querySelector('ul')
+// let list = document.querySelectorAll('li')
+
+// div.addEventListener('click', function(){
+//     console.log(" Div was clicked")
+// })
+
+// ul.addEventListener('click', function(event){
+//     event.stopPropagation() // To stop event bubbling
+//     console.log(" Ul was clicked ")
+// })
+
+// for (items of list){
+//     items.addEventListener('click', function(event){
+//     event.stopPropagation() // To stop event bubbling
+//     console.log(" Li was clicked ")
+// })
+// }
+
+//******************* Todo **********************
+
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
 let ul = document.querySelector('ul')
-let list = document.querySelectorAll('li')
 
-div.addEventListener('click', function(){
-    console.log(" Div was clicked")
+// ADDING ELEMENTS IN TODO
+btn.addEventListener('click', function(){
+    li = document.createElement('li');
+    li.innerText = input.value
+    input.value = ""
+    ul.appendChild(li)
+
+    // ADDING DELETE BUTTON
+    delBtn = document.createElement('button');
+    delBtn.innerText = "Delete"
+    delBtn.classList.add('delete') // needed for part-2 logic
+    li.appendChild(delBtn)
+    
 })
 
-ul.addEventListener('click', function(event){
-    event.stopPropagation() // To stop event bubbling
-    console.log(" Ul was clicked ")
+ul.addEventListener('click',function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItems = event.target.parentElement
+        listItems.remove()
+        console.log("Deleted")
+    }
 })
 
-for (items of list){
-    items.addEventListener('click', function(event){
-    event.stopPropagation() // To stop event bubbling
-    console.log(" Li was clicked ")
-})
-}
+// part -2
+// LOGIC ONLY APPLICABLE ON EXISTIING LIST ITEMS
+
+// let delBtns = document.querySelectorAll('.delete')
+// for(delBtn of delBtns){
+//     delBtn.addEventListener('click', function(){
+//     let par = this.parentElement;
+//     par.remove();
+//     })
+// }
