932574acf07723febc0d8f706f189f9139d72a82
Rahul Shaw <rahulshaw903866@gmail.com>
2025-07-10 22:57:16 +0530

    completed college name tracker

--- JAVASCIPT - 13/activity.js
+++ JAVASCIPT - 13/activity.js
@@ -1,26 +1,42 @@
-let url = "http://universities.hipolabs.com/search?name=";
-let input = document.querySelector("input");
+// let url = "http://universities.hipolabs.com/search?name=";
+// let input = document.querySelector("input");
 
+// let btn = document.querySelector("button");
+// let ul = document.querySelector("#list");
+
+// btn.addEventListener("click", async () => {
+//   let country = input.value;
+//   console.log(country);
+//   let colleges = await getColleges(country);
+//   show(colleges);
+//   input.value = "";
+// });
+
+// function show(colleges) {
+//   ul.innerText = "";
+
+//   for (clg of colleges) {
+//     let li = document.createElement("li");
+//     li.innerText = clg.name;
+//     ul.append(li);
+//   }
+// }
+
+// async function getColleges(country) {
+//   try {
+//     let res = await axios.get(url + country);
+//     return res.data;
+//   } catch (error) {
+//     console.log(error);
+//     return "No college found";
+//   }
+// }
+
+let url = "http://universities.hipolabs.com/search?name=";
+
+let input = document.querySelector("input");
 let btn = document.querySelector("button");
 let ul = document.querySelector("#list");
-
-btn.addEventListener("click", async () => {
-  let country = input.value;
-  console.log(country);
-  let colleges = await getColleges(country);
-  show(colleges);
-  input.value = "";
-});
-
-function show(colleges) {
-  ul.innerText = "";
-
-  for (clg of colleges) {
-    let li = document.createElement("li");
-    li.innerText = clg.name;
-    ul.append(li);
-  }
-}
 
 async function getColleges(country) {
   try {
@@ -28,6 +44,21 @@ async function getColleges(country) {
     return res.data;
   } catch (error) {
     console.log(error);
-    return "No college found";
+  }
+}
+
+btn.addEventListener("click", async () => {
+  let country = input.value;
+  let clgData = await getColleges(country);
+  show(clgData);
+  input.value=""
+});
+
+function show(clgData) {
+  ul.innerText = ""
+  for (clgs of clgData) {
+    let li = document.createElement("li");
+    li.innerText = clgs.name;
+    ul.append(li)
   }
 }
