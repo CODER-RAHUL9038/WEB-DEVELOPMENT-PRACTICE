8335492350df1a7a85ebf7fb928e459a65866195
Rahul Shaw <rahulshaw903866@gmail.com>
2025-07-19 22:57:43 +0530

    installed first npm package FIGLET

---
+++ BACKEND-1/figletDir/index.js
@@ -0,0 +1,9 @@
+const figlet = require('figlet');
+figlet("THANK YOU FOR YOUR SUPPORT!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
