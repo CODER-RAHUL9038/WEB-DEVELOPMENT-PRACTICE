049c10acbb926f3f38906908c11d88e38a75b6d7
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-06 23:57:38 +0530

    making instagram dynamic page using ejs

--- BACKEND-3/EJSDir/index.js
+++ BACKEND-3/EJSDir/index.js
@@ -1,8 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
 
 const port = 3000;
 
-app.listen(port , ()=> {
-    console.log(`Listening on port : ${port}`)
-})
+// SETTING EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.listen(port, () => {
+  console.log(`Listening on port : ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+//PARSING DATA IN EJS USING DICE ROLL EXAMPLE
+app.get("/roledice", (req, res) => {
+  let diceValue = Math.floor(Math.random() * 7);
+  //   res.render("roleDice.ejs", { num: diceValue }); // passing second argument as object whose key will be used in parsing data in ejs
+  //   res.render("roleDice.ejs", { diceValue: diceValue }); //same key name can be used
+  res.render("roleDice.ejs", { diceValue }); //Above line can be written as single value also
+});
+
+// CREATING INSTGRAM PAGE UISNG EJS
+app.get("/:ig/:username", (req, res) => {
+  let { username } = req.params;
+  let instaData = require("./data.json");
+  let data = instaData[username];
+  res.render("instagram.ejs", { data });
+});
