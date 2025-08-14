8e530a03d111757170a7b370183994abf656ab6c
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-14 23:11:39 +0530

    setting layout to create my first API

--- BACKEND-4/REST_CLASS/index.js
+++ BACKEND-4/REST_CLASS/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require('path') // importing path module
+const port = 8080;
 
-port = 8080;
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "Views"))
+app.set(express.static(path.join(__dirname, "/public")))
+
+app.use(express.urlencoded({extended : true})) // setting middleware for data parsing 
+
+app.get("/" , (req ,res) =>{
+  res.send("Server working well")
+})
+
 app.listen(port, (req, res) => {
   console.log(`listening on port : ${port}`);
 });
