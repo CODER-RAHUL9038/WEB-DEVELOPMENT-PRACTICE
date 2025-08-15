73c2ba84f177291820914812dc0630b5779efc47
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-15 23:55:36 +0530

    Created  all posts api route

--- BACKEND-4/REST_CLASS/index.js
+++ BACKEND-4/REST_CLASS/index.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const app = express();
-const path = require('path') // importing path module
+const path = require("path"); // importing path module
 const port = 8080;
 
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "Views"))
-app.set(express.static(path.join(__dirname, "/public")))
+app.use(express.urlencoded({ extended: true })); // setting middleware for data parsing
 
-app.use(express.urlencoded({extended : true})) // setting middleware for data parsing 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "Views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/" , (req ,res) =>{
-  res.send("Server working well")
+let posts = [
+  {
+    username: "Rahulshaw",
+    content: "I love coding",
+  },
+  {
+    username: "Scaduu",
+    content: " This is my first API creation",
+  },
+  {
+    username: "Birju",
+    content: " I am the training head of the program",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req,res) =>{
+  console.log(req.body)
+  res.send("post request working")
 })
 
 app.listen(port, (req, res) => {
