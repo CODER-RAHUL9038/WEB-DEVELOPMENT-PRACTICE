5e72107a0fb1096cea8b48b3b10b03e35702a10a
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-18 23:18:58 +0530

    Initialised READ operation Successfully

--- BACKEND PRACTICE/REST-CLASS/index.js
+++ BACKEND PRACTICE/REST-CLASS/index.js
@@ -1,12 +1,44 @@
+// REQURING ALL THE LIBRARIES, PACKAGES AND MODULES
 const express = require("express");
 const app = express();
+const path = require("path");
 
 port = 8080;
 
+// SETTING SERVER
 app.listen(port, (req, res) => {
   console.log(`Listening at port : ${port}`);
 });
 
+// SETTING VIEW ENGINE
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// MIDDLEWARES
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//  GET ROUTE - SHOWING ALL POSTS
 app.get("/posts", (req, res) => {
-  res.send("server working well");
+  res.render("home.ejs", { posts });
 });
+
+// RAW DATA ANALOGOUS TO DATABASES
+let posts = [
+  {
+    id: 123,
+    username: "@rahul",
+    content: "Creating Quoro",
+  },
+  {
+    id: 121,
+    username: "@rahuldev",
+    content: "Using Restful Api ",
+  },
+  {
+    id: 113,
+    username: "@rahulWeb",
+    content: "Performing CRUD operations",
+  },
+];
