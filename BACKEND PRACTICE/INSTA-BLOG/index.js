5ba3c74e21f570b2865e5e33d2bd1789bdb602cd
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-25 23:54:46 +0530

    added middlewares and started server

--- BACKEND PRACTICE/INSTA-BLOG/index.js
+++ BACKEND PRACTICE/INSTA-BLOG/index.js
@@ -1,2 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Setting middlewares
+// Setting EJS
+app.set("view ngine", "ejs");
+app.set("view", path.join(__dirname, "Views"));
+
+// Setting middlewares
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+port = 8080;
+
+// Starting server
+
+app.listen(port, (req, res) => {
+  console.log(`Listening on port : ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Get request working on port 8080");
+});
