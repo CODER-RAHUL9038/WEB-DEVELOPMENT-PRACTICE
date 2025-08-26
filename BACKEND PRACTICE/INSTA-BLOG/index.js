8cb42159f97c31fb30f624fe932d5f70d8b18967
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-26 22:27:44 +0530

    created home page and added styling

--- BACKEND PRACTICE/INSTA-BLOG/index.js
+++ BACKEND PRACTICE/INSTA-BLOG/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Setting middlewares
 // Setting EJS
 app.set("view ngine", "ejs");
-app.set("view", path.join(__dirname, "Views"));
+app.set("views", path.join(__dirname, "Views"));
 
 // Setting middlewares
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,10 +15,36 @@ port = 8080;
 
 // Starting server
 
+
+
 app.listen(port, (req, res) => {
   console.log(`Listening on port : ${port}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Get request working on port 8080");
+
+let posts = [
+  {
+    id :"sd",
+    username : "@rahul",
+    content : "Creating Instablog",
+    image : "image"
+  },
+  {
+    id :"sff",
+    username : "@rahul",
+    content : "Creating Instablog with image",
+    image : "image"
+  },
+  {
+    id :"dsd",
+    username : "@rahul",
+    content : "Will learn uploading image",
+    image : "image"
+  }
+]
+
+
+
+app.get("/posts", (req, res) => {
+  res.render("home.ejs", {posts})
 });
