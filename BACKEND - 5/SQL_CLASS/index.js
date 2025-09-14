04357654adf06922c5427b2c6efcd4da42388fe1
Rahul Shaw <rahulshaw903866@gmail.com>
2025-09-14 21:10:37 +0530

    learnt inserting data from code integration

--- BACKEND - 5/SQL_CLASS/index.js
+++ BACKEND - 5/SQL_CLASS/index.js
@@ -1,24 +1,40 @@
 const { faker } = require("@faker-js/faker");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-// create a connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Jaicoder123@",
-  database: "delta_app",
-});
+async function main() {
+  try {
+    // create a connection
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Jaicoder123@",
+      database: "delta_app",
+    });
 
-try {
-  connection.query("show tables", (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-} catch (error) {
-  console.log(error);
+    //INSERTING NEW DATA
+    const commands = "insert into user(id, username, email, password) values ?";
+    const users = [
+      [12238, "12368_newuser", "abnbcg@gmail.com", "1228abc"],
+      [1234, "1234_newuser", "abc@gmail.com", "126abc"],
+    ];
+
+    //DESTRUCTURING DATA AFTER EXECUTION
+    const [rows] = await connection.query(commands, [users]);
+    // TO ONLY SEE THE TALBE NAMES
+    // const row = rows.map((r) => Object.values(r)[0])
+    // console.log(row);
+    console.log(rows);
+    // console.log(fields); //metadata
+    // console.log("array length :", rows.length);
+    await connection.end();
+  } catch (error) {
+    console.log(error);
+  }
 }
-connection.end();
 
+main();
+
+//USING FAKER
 let getRandomUser = () => {
   return {
     id: faker.string.uuid(),
