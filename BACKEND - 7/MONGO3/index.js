5c3cf5c7048b31eee74314214a186727800ba387
Rahul Shaw <rahulshaw903866@gmail.com>
2025-11-10 18:02:59 +0530

    created Whatsapp db

--- BACKEND - 7/MONGO3/index.js
+++ BACKEND - 7/MONGO3/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 main()
   .then((res) => {
@@ -17,6 +18,22 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/Whatsapp");
 }
+
+let chat1 = new Chat({
+  from: "Elon Musk",
+  to: "Rahul Shaw",
+  msg: "Please send me your resume",
+  created_at: new Date(),
+});
+
+chat1
+  .save()
+  .then((res) => {
+    console.log("✅Saved to DB:", res);
+  })
+  .catch((err) => {
+    console.log("❌Error saving In DB:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Root working");
