18cef69c65b8b7b0cfd085f6a7dacb997c4c523c
Rahul Shaw <rahulshaw903866@gmail.com>
2025-11-22 22:06:38 +0530

    learnt middlewares and error handling

---
+++ BACKEND-8/MIDDLEWARES/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const ExpressError = require("./expressError.js");
+
+// app.use((req, res, next) => {
+//   // middleware interfaring betweeen req and res
+//   console.log("I am first middleware");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   // middleware interfaring betweeen req and res
+//   console.log("I am second middleware");
+//   next();
+// });
+
+// //Logger function
+// app.use((req, res, next) => {
+//   req.time = new Date().toLocaleDateString();
+//   console.log(req.method, req.hostname, req.path, req.time);
+//   next();
+// });
+
+const checkToken = (req, res, next) => {
+  let { token } = req.query;
+  if (token === "giveaccess") {
+    return next();
+  }
+
+  console.log("Hacking attempt.....");
+  throw new ExpressError(401, "Access Denied");
+};
+
+app.get("/api", checkToken, (req, res) => {
+  console.log("Data provided of X user");
+  res.send("This is the Private Data of out valuable customer");
+});
+
+app.use("/admin",(req,res)=>{
+    throw new ExpressError(403,"Access to Admin is forbidden")
+})
+
+app.get("/test", (req, res) => {
+  res.send("Hi i am root");
+});
+
+app.listen(8080, (req, res) => {
+  console.log("✅Server listening to port: 8080");
+});
