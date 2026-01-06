23e770c122f47931f088c54050df4179b0c8f930
Rahul Shaw <rahulshaw903866@gmail.com>
2026-01-06 22:57:53 +0530

    conditional rendering

--- REACT/basic-react-app/src/Product.jsx
+++ REACT/basic-react-app/src/Product.jsx
@@ -1,10 +1,11 @@
 import "./Product.css";
 
-function Product({ title, price }) {
+function Product({ title, price = 1, features, feature2 }) {
   return (
     <div className="Product">
       <h2>{title}</h2>
       <p>{price}</p>
+      {price > 2000 && <p>Discount of 5 %</p>}
     </div>
   );
 }
--- REACT/basic-react-app/src/ProductTab.jsx
+++ REACT/basic-react-app/src/ProductTab.jsx
@@ -1,11 +1,16 @@
 import Product from "./Product";
 
 function ProductTab() {
+  let options = ["Nice", "Durable", "Hardy"];
+  let optionObj = {
+    name: "Rahul",
+    age: 25,
+  };
   return (
     <>
-      <Product title="Phone" price="$300" />
-      <Product title="Laptop" price="2199" />
-      <Product title="IPhone" price="199999" />
+      <Product title="Phone" price={300} />
+      <Product title="Laptop" price={2999} />
+      <Product title="IPhone" />
     </>
   );
 }
