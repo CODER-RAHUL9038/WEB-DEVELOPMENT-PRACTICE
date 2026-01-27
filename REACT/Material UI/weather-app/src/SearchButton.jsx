d68ad37c1460d1dae2fcf45c570b520283facb70
Rahul Shaw <rahulshaw903866@gmail.com>
2026-01-27 23:50:32 +0530

    created searchButton component

---
+++ REACT/Material UI/weather-app/src/SearchButton.jsx
@@ -0,0 +1,31 @@
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+export default function SearchButton({ city, setCity, getWeather }) {
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(city);
+    setCity("");
+    getWeather();
+  };
+  return (
+    <div>
+      <h1>Search Box</h1>
+      <form onSubmit={handleSubmit} action="/">
+        <TextField
+          id="outlined-basic"
+          label="City"
+          variant="outlined"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <br />
+        <br />
+        <Button type="submit" variant="contained">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+}
